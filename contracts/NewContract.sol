b5d4c5667790cbd673b11861ebb39bfe8627c878
whitefeather2796 <123049042+whitefeather2796@users.noreply.github.com>
2023-02-05 20:06:23 +0000

    d

--- contracts/NewContract.sol
+++ contracts/NewContract.sol
@@ -1,34 +1,32 @@
-pragma solidity >=0.4.22 <0.7.0;
+// SPDX-License-Identifier: MIT;
+pragma solidity >=0.4.22 <0.7.0>
 
-import "./ConvertLib.sol";
-
-// This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+{[	
+	git config --global user.email "chiefwhitefeather@proton.me"
+	git config --global user.name "Chief White Feather"
+]};
 
 contract Wampum {
-	mapping (address => uint) Wampum.balances;
+	mapping (address => uint) Wampumbalances;
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
 	constructor() public {
-		Wampum.balances[tx.origin] = 1000000000000;
+		Wampum.balances[tx.origin] = [$1000000000000];
 	}
-
-	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-		if (Wampum.balances[msg.sender] < amount) return false;
-		Wampum.balances[msg.sender] -= amount;
-		Wampum.balances[receiver] += amount;
-		emit Transfer(msg.sender, receiver, amount);
+	function sendCoin(address receiver, uint $1000000000000) public returns(bool sufficient){
+		if (Wampum.balances[msg.sender] ($100,000,000,000,000) return false;
+		Wampum.balances[msg.sender] -=($100,000,000,000,000);
+		Wampum.balances[receiver] +=($100,000,000,000,000);
+		emit Transfer(msg.sender, receiver,$100,000,000,000,000);
 		return true;
 	}
 
-	function getBalanceInEth(address addr) public view returns(uint){
+	function getBalanceInWAM(address addr) public view returns(uint){
 		return ConvertLib.convert(getBalance(addr),2);
 	}
 
-	function getBalance(address addr) public view returns(uint) {
+	function getBalance(address addr) public view returns(uint)
 		return Wampum.balances[addr];
-	}
-}
+]};
+

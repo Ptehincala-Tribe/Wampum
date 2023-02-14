7c48241f75bf1476ad19f082c53ef8280f1a04fd
whitefeather2796 <123049042+whitefeather2796@users.noreply.github.com>
2023-02-13 23:59:21 -0600

    h

--- contracts/NewContract.sol
+++ contracts/NewContract.sol
@@ -1,32 +1,27 @@
-// SPDX-License-Identifier: MIT;
-pragma solidity >=0.4.22 <0.7.0>
-
-{[	
-	git config --global user.email chiefwhitefeather@proton.me
-	git config --global user.name Chief White Feather
-]};
-
-contract Wampum {
-	mapping (address => uint) Wampumbalances;
-
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-	constructor() public {
-		Wampum.balances[tx.origin] = [$1000000000000];
-	}
-	function sendCoin(address receiver, uint $1000000000000) public returns(bool sufficient){
-		if (Wampum.balances[msg.sender] ($100,000,000,000,000) return false;
-		Wampum.balances[msg.sender] -=($100,000,000,000,000);
-		Wampum.balances[receiver] +=($100,000,000,000,000);
-		emit Transfer(msg.sender, receiver,$100,000,000,000,000);
-		return true;
-	}
-
-	function getBalanceInWAM(address addr) public view returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
-	}
-
-	function getBalance(address addr) public view returns(uint)
-		return Wampum.balances[addr];
-]};
+pragma solidity >=0.4.22 <0.7.0>;
+{
+	[	
+	git config --global user.email "Chiefwhitefeather@proton.me"
+	git config --global user.name "Chief White Feather"
+    ]
+};
+{
+	[
+contract Wampum 
+mapping (address) => uint Wampum.balances
+event Transfer(address) indexed _from, address indexed _to, uint256 _value)
+constructor public
+			Wampum.balances[tx.origin] = [$100,000,000,000]
+			function sendCoin(address) receiver, uint [$100,000,000,000]
+			public returns (bool sufficient) if (Wampum.balances)[msg.sender] [$100,000,000,000] 
+			return (false)
+			Wampum.balances[msg.sender] =[-$100,000,000,000,000]
+			Wampum.balances[receiver] =[+$100,000,000,000,000]
+			emit Transfer[msg.sender] [receiver],[$100,000,000,000,000]return (true)
+function getBalanceInWAM(address addr) public view returns (uint)
+		return ConvertLib.convert(getBalance)(addr)
+function getBalance(address addr) public view returns (uint)
+		return Wampum.balances[addr]
+	]
+};
 

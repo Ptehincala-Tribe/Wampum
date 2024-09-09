3ecfc1abc846be98a9ef99cb1816256628a31293
whitefeather2796 <123049042+whitefeather2796@users.noreply.github.com>
2024-09-09 04:12:09 +0000

    love

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-function main(params) {
-    const msg = 'Hello, ' + params.name + ' from ' + params.place;
-    const family = 'You have ' + params.children + ' children ';
-    const stats = 'and are ' + params.height + ' m. tall.';
-    return { greeting: msg, details: family + stats };
-}
-module.exports.main = main;
+const { app } = require('@azure/functions');
+
+app.setup({
+    enableHttpStream: true,
+});

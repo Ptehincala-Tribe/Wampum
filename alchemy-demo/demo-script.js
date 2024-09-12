c1199aa29cfc3ecf6fdbd122989ad2045bfe8fda
whitefeather2796 <123049042+whitefeather2796@users.noreply.github.com>
2024-09-12 17:03:32 +0000

    h

--- alchemy-demo/demo-script.js
+++ alchemy-demo/demo-script.js
@@ -12,4 +12,36 @@ curl --request POST \
                                   	  false
                                         ]
                                         }
-                                        '
+                                        'const url = "https://eth-mainnet.g.alchemy.com/v2/arr2T-70UlYa-_TEGS93Q2_0vQ_act_c";
+                                        const headers = {
+                                          Accept: "application/json",
+                                            "Content-Type": "application/json",
+                                            };
+
+                                            const body = JSON.stringify({
+                                              id: 1,
+                                                jsonrpc: "2.0",
+                                                  method: "eth_getBlockByNumber",
+                                                    params: ["finalized", false],
+                                                    });
+
+                                                    fetch(url, {
+                                                      method: "POST",
+                                                        headers: headers,
+                                                          body: body,
+                                                          })
+                                                            .then((response) => response.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
